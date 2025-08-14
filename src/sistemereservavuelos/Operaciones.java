f74e8b60bd92a12e778e3897c228e903dad5dd63
jaine <jaine@JASA.lan>
2025-08-13 19:22:46 -0600

    Primer commit "Hice el proyecto SistemaReservaVuelos, ademas agregue tres clases para el trabajo, ConexionBD para la conexion a la base de datos, el main para ejecutar y la operaciones esta clase es la que contiene todas las funciones para buscar vuelos, crear reservas y consultar itinerarios"

---
+++ src/sistemereservavuelos/Operaciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemereservavuelos;
+
+
+/**
+ *
+ * @author jainer Said Garcia Gonzalez
+ */
+
+public class Operaciones {
+
+   
+}
